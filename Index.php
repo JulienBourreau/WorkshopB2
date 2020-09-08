97f8cba6903ead6fd977cf1eb54ed4f4caf3ef3d
Korgannon <Ascentia26@gmail.com>
2020-09-08 14:06:15 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,4 +1,25 @@
 <?php include_once "header.php" ?>
 <?php include_once "menu.php" ?>
 <h1>Consommation Energétique</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Espèce</th>
+            <th>Bio/Pas bio</th>
+            <th>Pays de provenance</th>
+            <th>Empreinte carbone</th>
+            <th>Score</th>
+        </tr>
+    </thead>
+	<tbody>
+		<tr>
+                <td>Pomme</td>
+                <td>Bio</td>
+                <td>France</td>
+                <td>12</td>
+                <td>10/10</td>
+            </tr>
+
+    </tbody>
+</table>
 <?php include_once "footer.php" ?>
