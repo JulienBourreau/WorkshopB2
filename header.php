b697624d54f62da8cd123c6e420cf0002878d139
Korgannon <Ascentia26@gmail.com>
2020-09-08 09:35:45 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Workshop Consommation Energétique</title>
+		<meta charset="UTF-8">
+		<title>Workshop Consommation Energétique</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="styles.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-
-</body>
-</html>
